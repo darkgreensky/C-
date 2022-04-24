b9eb1e5b330422e2a4b4e2d98355f41bffe539ed
darkgreensky <ghostgreentower@outlook.com>
2022-04-24 13:18:17 +0800

    析构函数生存周期

--- c.cpp
+++ c.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
-
 using namespace std;
-
-class Complex
+class Demo
 {
+	int id;
 public:
-    double real, imag;
-    Complex(int i) // 类型转换构造函数
-    {
-        cout << "IntConstructor" << endl;
-        real = i;
-        imag = 0;
-    }
-    Complex(double r, double i)
-    {
-        real = r;
-        imag = i;
-    }
+	Demo(int i)
+	{
+		id = i;
+		cout << "id = " << id << " constructed" << endl;
+	}
+	~Demo()
+	{
+		cout << "id = " << id << " destructed" << endl;
+	}
 };
-
+Demo d1(1);
+void Func()
+{
+	static Demo d2(2);
+	Demo d3(3);
+	cout << "func" << endl;
+}
 int main()
 {
-    Complex c1(7, 8);
-    Complex c2 = 12;
-    c1 = 9; // 9被自动转换成一个临时Complex对象
-    cout << c1.real << "," << c1.imag << endl;
-    return 0;
+	Demo d4(4);
+	d4 = 6;
+	cout << "main" << endl;
+	{
+		Demo d5(5);
+	}
+	Func();
+	cout << "main ends" << endl;
+	return 0;
 }
