f7aa063bfae51ec283ad652e314927c8c666124d
darkgreensky <ghostgreentower@outlook.com>
2022-05-12 19:57:50 +0800

    深拷贝和浅拷贝

--- c.cpp
+++ c.cpp
@@ -1,42 +1,15 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-using namespace std;
-class String
-{
-private:
-	char *str;
 
-public:
-	String() : str(NULL) {}
-	const char *c_str() const { return str; };
-	String &operator=(const char *s);
-	~String();
-};
-String &String::operator=(const char *s)
-{
-	if (str)
-		delete[] str;
-	if (s)
-	{
-		str = new char[strlen(s) + 1];
-		strcpy(str, s);
-	}
-	else
-		str = NULL;
-	return *this;
-}
-String::~String()
-{
-	if (str)
-		delete[] str;
-}
+using namespace std;
+
 int main()
 {
-	String s;
-	s = "Good Luck,";
-	cout << s.c_str() << endl;
-	s = "Shenzhou 8!";
-	cout << s.c_str() << endl;
+	string s1, s2;
+	s1 = "string";
+	s2 = s1;
+	s2[0] = 't';
+	cout << s1 << endl;
+	cout << s2 << endl;
 	return 0;
 }
